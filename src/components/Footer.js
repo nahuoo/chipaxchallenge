5c990b476d6c09a34b11ce5ee4f55c1b50e640f0
Nahuel Sigismondi <nahuel.sigismondi@gmail.com>
2021-11-24 20:42:23 -0300

    Fix: Fixed style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,61 +1,88 @@
 import {
   Box,
-  chakra,
   Container,
+  SimpleGrid,
+  chakra,
   Stack,
   Text,
+  Flex,
   useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react'
 import { FaGithub } from 'react-icons/fa'
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  )
-}
 
 export const Footer = () => {
-  return (
-    <Box bg="gray.50" color="gray.200">
-      <Container
-        as={Stack}
-        maxW={'6xl'}
-        py={4}
-        direction={{ base: 'column', md: 'row' }}
-        spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}
+    const SocialButton = ({
+      children,
+      label,
+      href,
+    }) => {
+      return (
+        <chakra.button
+          bg={useColorModeValue('black', 'whiteAlpha.100')}
+          rounded={'full'}
+          ml={2}
+          w={8}
+          h={8}
+          cursor={'pointer'}
+          as={'a'}
+          href={href}
+          display={'inline-flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          transition={'background 0.3s ease'}
+          _hover={{
+            bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+          }}
+        >
+          <VisuallyHidden>{label}</VisuallyHidden>
+          {children}
+        </chakra.button>
+      )
+    }
+    return (
+      <Box
+        pl={{ base: 0, md: '15%' }}
+        bg={useColorModeValue('gray.50', 'gray.900')}
+        color={useColorModeValue('gray.700', 'gray.200')}
       >
-        <Text>Nahuel Sigismondi</Text>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'Github'} href={'https://github.com/nahuoo'}>
-            <FaGithub />
-          </SocialButton>
-        </Stack>
-      </Container>
-    </Box>
-  )
+        <Container as={Stack} maxW={'6xl'} py={10}>
+          <SimpleGrid
+            columns={{ base: 1, sm: 2, md: 4 }}
+            spacing={8}
+          ></SimpleGrid>
+        </Container>
+        <Box py={10}>
+          <Flex
+            align={'center'}
+            _before={{
+              content: '""',
+              borderBottom: '1px solid',
+              borderColor: useColorModeValue('gray.200', 'gray.700'),
+              flexGrow: 1,
+              mr: 8,
+            }}
+            _after={{
+              content: '""',
+              borderBottom: '1px solid',
+              borderColor: useColorModeValue('gray.200', 'gray.700'),
+              flexGrow: 1,
+              ml: 8,
+            }}
+          >
+            Nahuel Sigismondi
+            <SocialButton label={'Github'} href={'https://github.com/nahuoo'}>
+              <FaGithub />
+            </SocialButton>
+          </Flex>
+          <Text pt={6} fontSize={'sm'} textAlign={'center'}>
+            © 2020 Chakra Templates. All rights reserved
+          </Text>
+        </Box>
+      </Box>
+    )
 }
+
+
+
