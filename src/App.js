8f8cc647343232f478cf89223a14ff5bced1697a
Nahuel Sigismondi <nahuel.sigismondi@gmail.com>
2021-11-24 21:15:45 -0300

    fix: Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import {
-  ChakraProvider,
-  theme,
-} from '@chakra-ui/react';
-import { Footer } from './components/Footer';
-import { Navbar } from './components/Navbar';
+import { ChakraProvider, theme } from '@chakra-ui/react'
+import { Footer } from './components/Footer'
+import { Home } from './components/Home'
+import { Navbar } from './components/Navbar'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      <Home />
       <Footer />
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
