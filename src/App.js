c49e80ef6002517ea3ae2a89cb895eca9d3a2d01
Nahuel Sigismondi <nahuel.sigismondi@gmail.com>
2021-11-24 21:13:26 -0300

    Fix: add Home to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { Footer } from './components/Footer';
+import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      <Home />
       <Footer />
     </ChakraProvider>
   );
