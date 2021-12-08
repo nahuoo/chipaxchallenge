4a2e312ed26599b8d92461c1317a3c00172bcddb
nahuoo <nahuel.sigismondi@gmail.com>
2021-12-08 16:10:22 -0300

    Create Modal.js

---
+++ src/components/Modal.js
@@ -0,0 +1,35 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+
+export const Modal = ({children}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>{children}</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{props.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {props.body}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Cerrar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
